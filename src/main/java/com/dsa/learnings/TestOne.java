571077f76acc4100afb0ad67dc001a60eb1ae7ee
Charan <charanbhavisetti2000@gmail.com>
2023-05-28 18:36:05 +0530

    Added timeElaspsed feature

--- src/main/java/com/dsa/learnings/TestOne.java
+++ src/main/java/com/dsa/learnings/TestOne.java
@@ -24,11 +24,27 @@ public class TestOne {
 
         }
 
+//        for(int i=0; i<arr1.length; i++) {
+//            for( int j=0; j < arr2.length; j++) {
+//                if ( arr1[i] == arr2[j]) {
+//                    return true;
+//                }
+//            }
+//        }
+
         return false;
     }
 
     public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+
         System.out.println(commonInTwoArraysExists());
+
+        long endTime = System.nanoTime();
+        double timeElapsed = (endTime - startTime) / 1000000.00  ;
+        System.out.println("Time Elapsed: " + timeElapsed);
+
     }
 
 }
