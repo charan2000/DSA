61bc9647f0c4905b1eedd5d9f4a4e9945b1fbcac
charan <charan.bhavisetti@fleetx.io>
2023-05-28 21:02:38 +0530

    Started mergeArrays & sorting program

--- src/main/java/com/dsa/learnings/TestOne.java
+++ src/main/java/com/dsa/learnings/TestOne.java
@@ -1,18 +1,14 @@
 package com.dsa.learnings;
 
-import javax.sound.midi.Track;
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class TestOne {
 
     private static boolean commonInTwoArraysExists() {
-
         int[] arr1 = new int[] { 2,5,7,3,89,0 };
         int[] arr2 = new int[] { 56,34,32,2,3 };
 
         HashSet<Integer> verifyMap = new HashSet<>();
-
         for(int i=0; i<arr1.length; i++ ) {
             if(!verifyMap.contains(arr1[i])) {
                 verifyMap.add(arr1[i]);
@@ -21,9 +17,7 @@ public class TestOne {
 
         for(int j=0; j < arr2.length; j++) {
             if (verifyMap.contains(arr2[j])) return true;
-
         }
-
 //        for(int i=0; i<arr1.length; i++) {
 //            for( int j=0; j < arr2.length; j++) {
 //                if ( arr1[i] == arr2[j]) {
@@ -31,20 +25,30 @@ public class TestOne {
 //                }
 //            }
 //        }
-
         return false;
     }
+
+    private static String stringReversal(String str) {
+        String newString = "";
+
+        for(int i=str.length()-1;i>=0;i--) {
+            // System.out.println(str.charAt(i));
+            newString += str.charAt(i);
+        }
+        return newString;
+    }
+
 
     public static void main(String[] args) {
 
         long startTime = System.nanoTime();
 
         System.out.println(commonInTwoArraysExists());
-
+        System.out.println(  stringReversal(":narahc mI sdnerp a&Y iH"));
         long endTime = System.nanoTime();
         double timeElapsed = (endTime - startTime) / 1000000.00  ;
+
         System.out.println("Time Elapsed: " + timeElapsed);
 
     }
-
 }
